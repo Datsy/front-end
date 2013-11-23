76e9ee3f8ddafc89e9bb7fdcc852866adf9ead44
Collen Jones <collenjones@gmail.com>
2013-11-23 12:01:50 -0800

    Set up YEOMAN Backbone

---
+++ public/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global public, $*/
+
+
+window.public = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    public.init();
+});
