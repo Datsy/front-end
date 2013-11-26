b3e216bf80b29c6dc7d2ec2ac913e88b4f76e25e
Collen Jones <collenjones@gmail.com>
2013-11-25 23:35:15 -0800

    Can add keyword tags and click to delete

--- public/javascript/views/ExploreDataView.js
+++ public/javascript/views/ExploreDataView.js
@@ -3,7 +3,8 @@ DatsyApp.ExploreDataView = Backbone.View.extend({
   className: '',
   
   events: {
-
+    'keypress :input': 'addPostKeyword',           // enter space bar in input box
+    'click span.delete-keyword': 'removeKeyword'
   },
 
   initialize: function() {
@@ -15,6 +16,24 @@ DatsyApp.ExploreDataView = Backbone.View.extend({
       .append(new DatsyApp.ExploreCategoriesView({model: this.model}).render())
       .append(new DatsyApp.ExploreDatasetsView({model: this.model}).render());
     return this;
+  },
+
+  addPostKeyword: function(e) {
+    var text = $('input').val();
+    // Add keyword if spacebar is entered
+    if(e.keyCode === 32) {
+      $('input').val('');
+      $('span.style').append(
+        '<span class="post-keyword">' +
+          text +
+          '<span class="delete-keyword" title="remove this keyword">x</span> \
+        </span>');
+    }
+  },
+
+  removeKeyword: function(e) {
+    $(e.target).parent().remove();
+    console.log('remove the span');
   }
 
 });
