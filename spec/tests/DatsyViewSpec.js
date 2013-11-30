e930e32b934980620ad6b09fe65d3c1a6f486270
Collen Jones <collenjones@gmail.com>
2013-11-29 16:27:10 -0800

    Tests that Backbone History has been started

--- spec/tests/DatsyViewSpec.js
+++ spec/tests/DatsyViewSpec.js
@@ -4,16 +4,28 @@ describe('DatsyAppView', function() {
 
   var datsyAppView = new DatsyApp.DatsyView({model: datsy});
 
-  it('should have a model', function() {
-    expect(datsyAppView.model).toBe(datsy);
-  });
+  describe('initialize', function() {
+    it('should have a model', function() {
+      expect(datsyAppView.model).toBe(datsy);
+    });
 
-  it('should have a template', function() {
-    expect(datsyAppView.template).toEqual(datsy.get('templates')['datsyApp']);
-  });
+    it('should have a template', function() {
+      expect(datsyAppView.template).toEqual(datsy.get('templates')['datsyApp']);
+    });
 
-  it('should generate a router', function() {
-    expect(datsyAppView.router).toEqual(jasmine.any(DatsyApp.Router));
+    it('should generate a router', function() {
+      expect(datsyAppView.router).toEqual(jasmine.any(DatsyApp.Router));
+    });
+
+    it('starts Backbone history', function() {
+      Backbone.history.started = null;
+      Backbone.history.stop();
+      spyOn(Backbone.history, "start");
+      datsyAppView.initialize();
+
+      expect(Backbone.history.start).toHaveBeenCalled();
+    });
+
   });
 
 });
