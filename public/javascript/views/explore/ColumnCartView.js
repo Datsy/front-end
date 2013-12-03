ffdf7665686574af02dde13c685967c900e66c55
David Ryan Hall <david.ryan.hall@gmail.com>
2013-12-03 09:02:00 -0800

    Hover over total columns in cart will display the selected columns

--- public/javascript/views/explore/ColumnCartView.js
+++ public/javascript/views/explore/ColumnCartView.js
@@ -1,5 +1,6 @@
 (function() {
   var _ref,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
@@ -7,6 +8,7 @@
     __extends(ColumnCartView, _super);
 
     function ColumnCartView() {
+      this.addColumn = __bind(this.addColumn, this);
       _ref = ColumnCartView.__super__.constructor.apply(this, arguments);
       return _ref;
     }
@@ -15,26 +17,52 @@
 
     ColumnCartView.prototype.events = {
       'click #clear': 'clearCart',
-      'click #go': 'loadVisualization'
+      'click #go': 'loadVisualization',
+      'mouseover .cart-text': 'viewSelectedColumns',
+      'mouseout .cart-text': 'hideSelectedColumns'
     };
 
     ColumnCartView.prototype.initialize = function(options) {
       this.datsyModel = options.datsyModel;
-      return this.template = this.datsyModel.get('templates')['columnCart'];
+      this.template = this.datsyModel.get('templates')['columnCart'];
+      this.datsyModel.on('addColumn', this.addColumn);
+      this.columnData = [];
+      return this.popoverContent = 'No columns selected';
     };
 
     ColumnCartView.prototype.render = function() {
+      var _this = this;
       this.$el.html(this.template);
+      setTimeout((function() {
+        return $('.cart-text').popover({
+          content: _this.popoverContent
+        });
+      }), 2);
       return this;
     };
 
     ColumnCartView.prototype.clearCart = function() {
       $('.total-columns-added').text('0');
+      this.popoverContent = 'No columns selected';
       return this.trigger('clearCart');
     };
 
     ColumnCartView.prototype.loadVisualization = function() {
       return this.trigger('loadVisualization');
+    };
+
+    ColumnCartView.prototype.viewSelectedColumns = function() {
+      $('.cart-text').popover('show');
+      return $('.popover-content').html(this.popoverContent);
+    };
+
+    ColumnCartView.prototype.hideSelectedColumns = function() {
+      return $('.cart-text').popover('hide');
+    };
+
+    ColumnCartView.prototype.addColumn = function(columnData) {
+      this.columnData.push(columnData);
+      return this.popoverContent = columnData.columnName;
     };
 
     return ColumnCartView;
--- resources/coffeescript/views/explore/ColumnCartView.coffee
+++ resources/coffeescript/views/explore/ColumnCartView.coffee
@@ -4,20 +4,37 @@ class DatsyApp.ColumnCartView extends Backbone.View
 
   events:
     'click #clear' : 'clearCart',
-    'click #go' : 'loadVisualization'
+    'click #go' : 'loadVisualization',
+    'mouseover .cart-text' : 'viewSelectedColumns',
+    'mouseout .cart-text' : 'hideSelectedColumns'
 
   initialize: (options) ->
     @datsyModel = options.datsyModel
     @template = @datsyModel.get('templates')['columnCart']
+    @datsyModel.on 'addColumn', @addColumn
+    @columnData = []
+    @popoverContent = 'No columns selected'
 
   render: ->
     @$el.html @template
+    setTimeout (=> $('.cart-text').popover {content: @popoverContent } ), 2
     @
 
   clearCart: ->
     $('.total-columns-added').text('0')
+    @popoverContent = 'No columns selected'
     @trigger 'clearCart'
 
   loadVisualization: ->
     @trigger 'loadVisualization'
-  
+
+  viewSelectedColumns: ->
+    $('.cart-text').popover('show')
+    $('.popover-content').html @popoverContent
+
+  hideSelectedColumns: ->
+    $('.cart-text').popover('hide')
+
+  addColumn: (columnData) =>
+    @columnData.push columnData
+    @popoverContent = columnData.columnName
