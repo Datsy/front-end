e00d34a5f2e0709dc042b7ffe1c1ef3fdf2128a6
David Ryan Hall <david.ryan.hall@gmail.com>
2013-12-07 17:26:53 -0800

    Filter page work

--- spec/tests/DatsyViewSpec.js
+++ spec/tests/DatsyViewSpec.js
@@ -1,23 +1,28 @@
 describe('DatsyAppView', function() {
  
   var datsy, datsyAppView;
+  datsy = new DatsyApp.Datsy();
+  datsy.set('templates', templates);
+  var click = jQuery.Event( "click" );
  
   beforeEach(function () {
-    datsy = new DatsyApp.Datsy();
-    datsy.set('templates', templates);
     datsyAppView = new DatsyApp.DatsyView({ model: datsy });
+  });
+
+  afterEach(function() {
+    Backbone.history.stop();
   });
 
   it('should have a model', function() {
     expect(datsyAppView.model).toBe(datsy);
   });
 
-  xit('should have a template', function() {
+  it('should have a template', function() {
     expect(datsyAppView.template).toEqual(datsy.get('templates')['datsyApp']);
   });
 
-  xit('should generate a router', function() {
-    expect(DatsyView.router).toEqual(jasmine.any(Datsy.Router));
+  it('should generate a router', function() {
+    expect(datsyAppView.router).toEqual(jasmine.any(DatsyApp.Router));
   });
 
 });
